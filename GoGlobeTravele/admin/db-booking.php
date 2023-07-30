fc65ad848eb0d40bde1c6b93fd51a35dc33ad09c
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-07-30 12:10:33 +0530

    active packages done.

--- GoGlobeTravele/admin/db-booking.php
+++ GoGlobeTravele/admin/db-booking.php
@@ -196,120 +196,55 @@
                                     <th>Destination</th>
                                     <th>Id</th>
                                     <th>status</th>
-                                    <th>Enquiry</th>
+                                    
                                     <th>People</th>
                                     <th>action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-success">Approve</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment2.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-primary">Pending</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment3.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-danger">Reject</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment4.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-primary">Pendding</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment5.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-danger">Reject</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="list-img"><img src="assets/images/comment6.jpg" alt="">
-                                        </span><span class="list-enq-name">John Doe</span>
-                                    </td>
-                                    <td>12 may</td>
-                                    <td>Japan</td>
-                                    <td>755</td>
-                                    <td><span class="badge badge-success">approve</span></td>
-                                    <td>
-                                        <span class="badge badge-success">15</span>
-                                    </td>
-                                    <td><span class="badge badge-success">9</span></td>
-                                    <td>
-                                        <span class="badge badge-success"><i class="far fa-edit"></i></span>
-                                        <span class="badge badge-danger"><i class="far fa-trash-alt"></i></span>
-                                    </td>
-                                </tr>
+                            <?php
+require_once "db_connection.php";
+
+$query = "SELECT pb.booking_ID, pb.check_in_date, pb.grp_size, p.title, p.city, p.status, u.fName
+FROM pack_booking pb
+LEFT JOIN packages p ON pb.pack_ID = p.pack_ID
+LEFT JOIN users u ON pb.user_ID = u.user_ID
+";
+
+$result = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_array($result)) {
+        $bookingID = $row['booking_ID'];
+        $checkinDate = $row['check_in_date'];
+        $grpSize = $row['grp_size'];
+        $title = $row['title'];
+        $city = $row['city'];
+        $status = ($row['status'] == 1) ? "Active" : "Pending";
+        $userName = $row['fName'];
+
+        echo "
+            <tr>
+                <td>
+                    <span class='list-enq-name'>$userName</span>
+                </td>
+                <td>$checkinDate</td>
+                <td>$city</td>
+                <td>$bookingID</td>
+                <td><span class='badge badge-success'>$status</span></td>
+               
+                <td><span class='badge badge-success'>$grpSize</span></td>
+                <td>
+                    <span class='badge badge-danger delete-btn' data-bookingid='$bookingID'><i class='far fa-trash-alt'></i></span>
+                </td>
+            </tr>
+        ";
+    }
+} else {
+    echo "<tr><td colspan='8'>No bookings found.</td></tr>";
+}
+?>
+
                             </tbody>
                         </table>
                     </div>
